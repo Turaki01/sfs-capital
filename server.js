690bd44b951b81ce574ddb96a40635a3ec13a0a0
Turaki01 <ezekiel_zakari@yahoo.com>
2021-09-09 11:46:50 +0100

    heroku failed build fix

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
-const express = require('express');
-const path = require('path');
 const app = express();
+let listenPort = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(9000);
+app.listen(listenPort, ()=> {
+  console.log("server running on port "+ listenPort);
+})
